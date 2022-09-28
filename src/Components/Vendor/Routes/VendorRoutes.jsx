67665ec6406ab291b3b034ad55793c8bf6f90af0
Madhusudan0906 <prajapatmadhusudan06@gmail.com>
2022-09-28 23:57:59 +0530

    added client page

--- src/Components/Vendor/Routes/VendorRoutes.jsx
+++ src/Components/Vendor/Routes/VendorRoutes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route, useParams } from "react-router-dom";
+import Clients from "../../../Pages/Clients/Clients";
 import Dashboard from "../Dashboard/Dashboard";
 const VendorRoutes = () => {
   const params = useParams();
@@ -7,7 +8,7 @@ const VendorRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
-      <Route path="/clients" element={<p>client</p>} />
+      <Route path="/clients" element={<Clients />} />
       <Route path="/projects" element={<p>Projects</p>} />
     </Routes>
   );
