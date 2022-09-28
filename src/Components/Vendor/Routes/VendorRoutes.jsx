a01260fd5491649eb5efd8b9fb2586b359b0bcfb
prashant giri <pgiri570@gmail.com>
2022-09-28 23:28:40 +0530

    day2 code added

--- src/Components/Vendor/Routes/VendorRoutes.jsx
+++ src/Components/Vendor/Routes/VendorRoutes.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Routes, Route, useParams } from "react-router-dom";
+import Projects from "../../../Pages/Projects/Projects";
 import Dashboard from "../Dashboard/Dashboard";
+
+
 const VendorRoutes = () => {
   const params = useParams();
 
@@ -8,7 +11,8 @@ const VendorRoutes = () => {
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/clients" element={<p>client</p>} />
-      <Route path="/projects" element={<p>Projects</p>} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/projects/:id" element={<h1>ID</h1>} />
     </Routes>
   );
 };
